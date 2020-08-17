a3b16a53a5399ffe480f23512cd02bd212c4d1ed
Paul Billington-Dykes <paul.billingtondykes@gmail.com>
2020-08-17 14:28:04 +0100

    Styling changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,17 +29,46 @@
       </div>
     </nav>
     <section class="main-content columns">
-      <div class="container column is-12">
-        <nuxt />
-      </div>
+      <nuxt />
     </section>
 
     <footer>
       <section class="main-content columns">
-        <aside class="column is-4 section">
-          Sapien Interactive &copy; {{ year }}
+        <aside class="column is-4 logoWrapper">
+          <LogoSmall class="logo" />
+          <p class="strapline">football's premier scouting game</p>
         </aside>
-        <aside class="column is-8 section links right">
+        <aside class="column is-8">
+          <div class="columns footerNav">
+            <div class="column is-4">
+              <p><strong>About</strong></p>
+              <ul>
+                <li><a href="#">Product</a></li>
+                <li><a href="#">Company</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+            <div class="column is-4">
+              <p><strong>Legal</strong></p>
+              <ul>
+                <li><a href="#">Imprint</a></li>
+                <li><a href="#">Terms</a></li>
+                <li><a href="#">Privcay Policy</a></li>
+              </ul>
+            </div>
+            <div class="column is-4">
+              <p><strong>Download</strong></p>
+              <ul>
+                <li><a href="#">App Store</a></li>
+                <li><a href="#">Google Play</a></li>
+              </ul>
+            </div>
+          </div>
+        </aside>
+      </section>
+      <section class="main-content columns social">
+        <aside class="column is-3 section">
+          <p>Follow Us</p>
           <a href="https://www.facebook.com/bootbag" target="_blank">
             <img src="~/assets/facebook.png"
           /></a>
@@ -47,6 +76,21 @@
           <a href="https://twitter.com/bootbag2" target="_blank"
             ><img src="~/assets/twitter.png"
           /></a>
+          <a href="https://twitter.com/bootbag2" target="_blank"
+            ><img src="~/assets/twitter.png"
+          /></a>
+          <a href="https://twitter.com/bootbag2" target="_blank"
+            ><img src="~/assets/twitter.png"
+          /></a>
+        </aside>
+        <aside class="column is-9 section download">
+          <a href="#"><img src="~/assets/BtnAppStore.png"/></a>
+          <a href="#"><img src="~/assets/BtnGooglePlay.png"/></a>
+        </aside>
+      </section>
+      <section class="main-content columns">
+        <aside class="column is-12 section">
+          Sapien Interactive &copy; {{ year }}
         </aside>
       </section>
     </footer>
@@ -119,15 +163,64 @@ export default {
 }
 
 footer {
-  border-top: solid 1px #292929;
   font-size: 14px;
+  background-image: url('../assets/images/Map@2x.png');
+  background-repeat: no-repeat;
+  background-position: top right;
+  padding: 100px 40px;
+  background-size: 50%;
 
-  .section {
-    padding: 2rem 1.5rem 1rem 1.5rem;
+  .logoWrapper {
+    vertical-align: top;
   }
 
-  .right {
-    text-align: right;
+  .strapline {
+    color: #fff;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  .social {
+    border-top: solid 1px #242424;
+
+    p {
+      font-weight: bold;
+      text-transform: uppercase;
+      margin-bottom: 10px;
+    }
+
+    a {
+      margin-right: 10px;
+    }
+  }
+
+  .download {
+    img {
+      width: 150px;
+      margin-top: 30px;
+    }
+  }
+
+  .footerNav {
+    p {
+      font-size: 18px;
+      color: #fff;
+      text-transform: uppercase;
+      margin: 55px 0 30px 0;
+
+      strong {
+        color: #fff;
+      }
+    }
+
+    ul {
+      li {
+        a {
+          color: #8d8a8a;
+          font-weight: bold;
+        }
+      }
+    }
   }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,85 @@
 <template>
-  <div>
-    <section class="videoPlaceholder">
-      VIDEO GOES HERE
-    </section>
-    <section class="section">
-      <div class="coming-soon">
-        <h1>Show rival scouts you are the best.</h1>
-        <hr />
-        <h2>
-          Select the Biggest Improving players across 14 Divisions for your
-          roster.
-        </h2>
-        <hr />
-        <p>Buy before they peak.<br />Sell before they drop.</p>
-        <p class="blue">Join the revolution. Register to become a scout.</p>
-        <div v-if="!sending && success">
-          <p>
-            You have Successfully Registrated for Bootbag, keep an eye on your
-            emails for more details!
-          </p>
+  <div class="main">
+    <div class="container column is-12">
+      <section class="videoPlaceholder">
+        VIDEO GOES HERE
+      </section>
+      <section class="section">
+        <div class="coming-soon">
+          <h1>
+            The ultimate platform for fans to scout and transfer football
+            players, allowing users to benefit from their performance with the
+            chance of financial rewards.
+          </h1>
         </div>
-        <form v-else @submit.prevent="sendEmail" class="container">
-          <div class="columns">
-            <input
-              v-model="email"
-              type="email"
-              class="column"
-              placeholder="Email Address"
-            />
-            <button :disabled="sending" type="submit" class="column">
-              <img v-if="sending" src="~/assets/spinner.svg" />
-              <span v-else="!sending">Register Now</span>
-            </button>
+        <div class="columns has-text-centered ticketWrapper">
+          <div class="column ticket">
+            <img src="~/assets/images/Search-player-icon@2x.png" />
+            <p class="text">
+              Football's very first,<br /><strong
+                >real-time scouting game.</strong
+              >
+            </p>
+            <p class="bottom">
+              18 Million UK Football Fans
+            </p>
           </div>
-        </form>
-        <p class="pink">Download soon on...</p>
-        <p class="download">
-          <img src="~/assets/BtnGooglePlay.png" />
-          <img src="~/assets/BtnAppStore.png" />
-        </p>
-      </div>
+          <div class="column ticket">
+            <img src="~/assets/images/Pick-Roster-icon@2x.png" />
+            <p class="text">
+              Choose from over
+              <strong>2600<br />players from across the world.</strong>
+            </p>
+            <p class="bottom">3.5 Billion Global Football Fans*</p>
+          </div>
+          <div class="column ticket">
+            <img src="~/assets/images/Watch-Match-Vision-icon@2x.png" />
+            <p class="text">
+              Watch the action.<br />Find the players,<br /><strong
+                >Beat the competition.</strong
+              >
+            </p>
+            <p class="bottom">Compete To Be The Best</p>
+          </div>
+        </div>
+        <div class="coming-soon">
+          <h2>
+            Register To Become A Scout.
+          </h2>
+        </div>
+        <div class="coming-soon">
+          <div v-if="!sending && success">
+            <p>
+              You have Successfully Registered for Bootbag, keep an eye on your
+              emails for more details!
+            </p>
+          </div>
+          <form v-else @submit.prevent="sendEmail" class="container">
+            <div class="columns">
+              <div class="column is-two-thirds">
+                <input
+                  v-model="email"
+                  type="email"
+                  class="column"
+                  placeholder="Email Address"
+                />
+              </div>
+              <div class="column">
+                <button :disabled="sending" type="submit" class="column">
+                  <img v-if="sending" src="~/assets/spinner.svg" />
+                  <span v-else="!sending">Register Now</span>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+    <section class="yellowSection  has-text-centered">
+      <p>
+        <span>Join The Revolution</span
+        ><strong>Register To Become A Scout</strong><a href="#">Register Now</a>
+      </p>
     </section>
   </div>
 </template>
@@ -94,14 +134,19 @@ export default {
   text-align: center;
 }
 
+.main {
+  width: 100%;
+}
+
 .coming-soon {
   display: flex;
   flex-direction: column;
   justify-items: center;
   align-items: center;
+  margin-top: 20px;
 
-  > * {
-    margin: 1rem 0px;
+  h1 {
+    width: 70%;
   }
 }
 
@@ -111,6 +156,14 @@ h1 {
   text-transform: uppercase;
   font-size: 18px;
   text-align: center;
+}
+
+h1 {
+  margin-bottom: 30px;
+}
+
+h2 {
+  font-weight: bold;
 }
 
 p.blue {
@@ -126,27 +179,30 @@ hr {
 form {
   width: 100%;
 
+  column {
+    padding: 0;
+  }
+
   input[type='email'] {
     width: 100%;
     padding: 10px;
     border: solid 1px #ffffff;
-    background-color: #000000;
-    color: #ffffff;
+    background-color: #ffffff;
+    color: #333333;
     font-size: 18px;
     display: inline-block;
-    margin-bottom: 20px;
+    margin-bottom: 0;
   }
 
   button[type='submit'] {
-    width: 100%;
     font-size: 18px;
-    background-color: #f80290;
-    border: solid 1px #f80290;
+    background-color: #267efc;
+    border: solid 1px #267efc;
     padding: 10px 20px;
     color: #ffffff;
-    border-radius: 5px;
     display: inline-block;
-    margin-bottom: 20px;
+    margin-bottom: 0;
+    width: 100%;
 
     img {
       max-height: 17px;
@@ -165,6 +221,78 @@ form {
   color: #f80290;
 }
 
+.ticketWrapper {
+  margin: 20px 0 40px 0;
+
+  .ticket {
+    background-color: #242323;
+    margin: 20px 20px;
+    padding: 10px 0 0 0;
+    border-radius: 5px;
+
+    p {
+      margin: 0;
+      padding: 10px 0;
+    }
+
+    img {
+      width: 60px;
+      margin-top: -60px;
+    }
+
+    .text {
+      height: 120px;
+      color: #fff;
+
+      strong {
+        color: #fff;
+      }
+    }
+
+    .seperator {
+      width: 100%;
+      margin: 0;
+      padding: 0;
+    }
+
+    .bottom {
+      margin: 0;
+      text-transform: uppercase;
+      background-color: #535353;
+      padding: 10px 0;
+      background-image: url('../assets/images/Group 1444@2x.png');
+      background-repeat: no-repeat;
+      background-position: top;
+      background-size: 100%;
+    }
+  }
+}
+
+.yellowSection {
+  background-color: #fff000;
+
+  p {
+    span {
+      padding: 7px 0;
+      color: #000;
+      display: inline-block;
+    }
+
+    strong {
+      border-left: solid 1px #000;
+      padding-left: 10px;
+      margin-left: 10px;
+    }
+
+    a {
+      background-color: #267efc;
+      color: #fff;
+      padding: 10px 20px;
+      margin-left: 20px;
+    }
+  }
+}
+
 @media only screen and (min-width: 560px) {
   form {
     width: 75%;
@@ -174,10 +302,14 @@ form {
 @media only screen and (min-width: 768px) {
   form {
     width: 50%;
+  }
 
-    button[type='submit'] {
-      margin-left: 10px;
-    }
+  input[type='email'] {
+    margin-bottom: 20px;
+  }
+
+  button[type='submit'] {
+    margin-bottom: 20px;
   }
 }
 </style>
